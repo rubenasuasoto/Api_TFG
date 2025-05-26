093ccda0ed53816eace352e7ee01cd5424b85a08
ruben asua <rubenasuasoto@gmail.com>
2025-05-26 15:39:15 +0200

    email y logs de pedido

--- src/main/kotlin/com/es/TFG/security/SecurityConfig.kt
+++ src/main/kotlin/com/es/TFG/security/SecurityConfig.kt
@@ -49,7 +49,7 @@ class SecurityConfig {
                 .requestMatchers(HttpMethod.DELETE, "/pedidos/self/{id}").authenticated()
 
                 .requestMatchers(HttpMethod.GET, "/pedidos").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.POST, "/pedidos").hasRole("ADMIN") // si usas también pedidos por admin
+                .requestMatchers(HttpMethod.POST, "/pedidos").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/pedidos/{id}").hasRole("ADMIN")
 
 
