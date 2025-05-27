bdae3354f8fc3d18d8e50d9777f275309159cb0e
ruben asua <rubenasuasoto@gmail.com>
2025-05-27 11:41:31 +0200

    .

--- src/main/kotlin/com/es/TFG/model/Factura.kt
+++ src/main/kotlin/com/es/TFG/model/Factura.kt
@@ -1,12 +1,13 @@
 ﻿package com.es.TFG.model
 
 import org.bson.codecs.pojo.annotations.BsonId
+import java.time.Instant
 import java.util.Date
 
 data class Factura(
     @BsonId
     val numeroFactura: String,
-    val fecha: Date
+    val fecha: Date = Date.from(Instant.now())
 )
 
 
